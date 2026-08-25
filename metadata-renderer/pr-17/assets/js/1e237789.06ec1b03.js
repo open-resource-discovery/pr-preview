91eb0629fee4b68900b9164ec5c0ac5ceca176b0
aneta-a <aneta-a@users.noreply.github.com>
2026-08-25 11:21:52 +0000

    Deploying to main from @ open-resource-discovery/metadata-renderer@f302c13c904103fdee1597f34df418d07a7f403c 🚀

---
+++ metadata-renderer/pr-17/assets/js/1e237789.06ec1b03.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([["3632"],{38775(e){e.exports=JSON.parse('{"metadata":{"permalink":"/pr-preview/metadata-renderer/pr-17/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":0,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
