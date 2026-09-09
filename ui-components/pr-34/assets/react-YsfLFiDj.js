27bf22d6451608cb0f55a8bf4b7f0044a943caed
aneta-a <aneta-a@users.noreply.github.com>
2026-09-09 15:37:40 +0000

    Deploying to main from @ open-resource-discovery/ui-components@ea097376e0242aa7fa1671742f169401a2e2ff30 🚀

---
+++ ui-components/pr-34/assets/react-YsfLFiDj.js
@@ -0,0 +1,1 @@
+import{n as e,t}from"./DocsRenderer-JROSPFPF-CswQq4_u.js";t();export{e as MDXProvider};
---
+++ ui-components/pr-34/assets/use-button-XyVk6u30.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-DrYiUz_o.js";import{t}from"./useButton-637KF5sN.js";var n=e((()=>{t()}));export{n as t};
