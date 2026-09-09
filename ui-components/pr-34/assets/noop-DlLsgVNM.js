991a458164b01831410427ba6e592ea9fdda1664
aneta-a <aneta-a@users.noreply.github.com>
2026-09-09 09:31:16 +0000

    Deploying to main from @ open-resource-discovery/ui-components@0c7bc21003271cf95893e9a23cfc0b3cb90c55ca 🚀

---
+++ ui-components/pr-34/assets/noop-DlLsgVNM.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-DrYiUz_o.js";import{f as t}from"./useRenderElement-DKqgbtqm.js";var n=e((()=>{t()}));export{n as t};
---
+++ ui-components/pr-34/assets/useValueChanged-CUbt_pGj.js
@@ -0,0 +1,1 @@
+import{i as e,s as t}from"./preload-helper-DrYiUz_o.js";import{N as n}from"./iframe-BL8MqmK1.js";import{i as r,n as i,r as a,t as o}from"./useIsoLayoutEffect-BaxDJJ0c.js";function s(e,t){let n=c.useRef(e),a=r(t);i(()=>{n.current!==e&&a(n.current),n.current=e},[e,a])}var c,l=e((()=>{c=t(n(),1),o(),a()}));export{s as n,l as t};
